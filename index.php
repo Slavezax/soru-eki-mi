b145f758a1c7bbdcfaf0115d82ef2e9d5142cda9
Berkan Cesur <berkan@bilemezsin.com>
2017-07-08 23:13:48 +0300

    Fonksiyon

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+function soruEkiGetir($metin){
+$sonHarf = mb_substr($metin,-2, 1,'UTF-8');
+
+if ($sonHarf=='a' || $sonHarf=='ı'){
+	$eklenecek =' mı?';
+} else if ($sonHarf=='e' || $sonHarf=='i'){
+	$eklenecek =' mi?';
+} else if ($sonHarf=='u' || $sonHarf=='o'){
+	$eklenecek =' mu?';
+} else if ($sonHarf=='ü' || $sonHarf=='ö'){
+	$eklenecek =' mü?';
+} else {
+	$eklenecek =' mi?';
+}
+
+$sonHal = $metin . $eklenecek;
+return $sonHal;
+}
+
